7bdf19ba1e0eaf3b6d07ff73131fa57e9e731a2d
MikeSteiner <mike.steiner.77@gmail.com>
2017-01-03 06:20:44 +0200

    with remote database in mongodb://admin:************@ds151108.mlab.com:51108/family-notes

--- server/config/index.js
+++ server/config/index.js
@@ -6,12 +6,14 @@ const rootPath = path.join(__dirname, "/../../");
 
 let connectionStrings = {
     production: process.env.CONNECTION_STRING,
-    development: "mongodb://localhost:27017/family-notes"
+    // development: "mongodb://localhost:27017/family-notes"
+    development: "mongodb://admin:admin@ds151108.mlab.com:51108/family-notes"
 };
 
 module.exports = {
     environment: process.env.NODE_ENV || "development",
-    connectionString: connectionStrings[process.env.NODE_ENV || "development"],
+    // connectionString: connectionStrings[process.env.NODE_ENV || "development"],
+    connectionString: connectionStrings["development"],
     port: process.env.PORT || 3003,
     rootPath,
     jwtSecret: "secretjwtomfgjeesuswtf"
